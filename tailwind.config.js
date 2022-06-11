26177a3a3965560fa839a49d7fb833a174faea82
katungi <dankatdennis@gmail.com>
2022-06-11 08:35:59 +0300

    - added favicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+      extend: {
+      colors: {
+        primary: '#B70569',
+        darkAccent: '#210D15'
+      },
+    },
   },
   plugins: [],
 }
