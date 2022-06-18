230b803bdb931702b1f7bfa70040c1b47d8826aa
John Gachihi <johngachihi3@gmail.com>
2022-06-18 15:45:24 +0000

    fix: move web fonts to _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,6 @@ const Home: NextPage = () => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-
-        {/* Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Poppins:wght@300;400&display=swap"
-              rel="stylesheet" />
       </Head>
 
       <main className="w-full px-4 md:px-8 lg:px-20">
