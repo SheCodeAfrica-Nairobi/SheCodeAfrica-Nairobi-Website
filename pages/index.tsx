0633d99742e6cc80605c38b8d26d1b78324193f0
steekam <waynewanyee@gmail.com>
2022-06-21 22:37:05 +0300

    ui: add events sections to home page
    - Based off the design, the cards only have image posters at the moment

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,23 @@ import Image from 'next/image'
 import { prependOnceListener } from 'process'
 import Banner from "../components/Banner";
 import Nav from '../components/Nav';
+import EventsPane from '../components/EventsPane'
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>She-Code-Nairobi</title>
+        <title>She Code Africa Nairobi Chapter</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
 
-      <main>
+      <main className="text-darkAccent">
         <Nav />
         <Banner />
+        <EventsPane/>
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-      fontFamily: {
-        header: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
-      },
-      extend: {
+    fontFamily: {
+      header: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
       colors: {
         primary: '#B70569',
-        darkAccent: '#210D15'
+        darkAccent: '#210D15',
+        gray: "#FEFEFE",
       },
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans]
