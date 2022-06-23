c887d4d97a2ba6f897117e9441a028774d0ca105
Mogakamo <mogaka.amo254@gmail.com>
2022-06-23 11:33:57 +0300

    working on the responsiveness

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import { FaUsers } from "react-icons/fa";
+import { AiOutlineRise } from "react-icons/ai";
+import { ImEye } from "react-icons/im";
+import { GiPerson } from "react-icons/gi";
 
 const CallToAction = () => {
   return (
-    <div className="mx-[100px] my-[100px] bg-primary bg-opacity-10 h-[446px] px-20 py-10">
+    <div className="md:mx-[100px] my-[100px] bg-primary bg-opacity-10 h-auto w-auto md:h-[446px] px-20 py-10">
       <div className="flex justify-between leading-loose items-center">
         <h2 className="font-medium text-2xl text-primary">We value:</h2>
-        <button className="border-primary text-primary border-2 rounded-full px-6 py-2">
+        <button
+          className="border-primary text-primary border-2 rounded-full 
+                    px-6 py-2"
+        >
           Be a member
         </button>
+      </div>
+      <div className="flex py-10 mx-auto space-x-3 md:space-x-6 lg:space-x-10 h-auto">
+        {/* Value 1 */}
+        <div className="space-y-4 text-darkAccent w-auto">
+          <FaUsers className="h-16 text-primary w-16" />
+          <h1 className="font-semibold">Team work</h1>
+          <p>
+            We are bringing together women in our community to work together and
+            empower each other.
+          </p>
+        </div>
+        {/* Value 2 */}
+        <div className="space-y-4 text-darkAccent w-auto">
+          <AiOutlineRise className="h-16 text-primary w-16" />
+          <h1 className="font-semibold">Technical Growth</h1>
+          <p>
+            We provide technical and non-technical sessions to help our members
+            grow their skills.
+          </p>
+        </div>
+        {/* Value 3 */}
+        <div className="space-y-4 text-darkAccent w-auto">
+          <ImEye className="h-16 text-primary w-16" />
+          <h1 className="font-semibold">Visibility</h1>
+          <p>
+            We create visibility for our members through volunteer opportunities
+            and speaking sessions.
+          </p>
+        </div>
+        {/* Value 4 */}
+        <div className="space-y-4 text-darkAccent w-auto">
+          <GiPerson className="h-16 text-primary w-16" />
+          <h1 className="font-semibold">Leadership</h1>
+          <p>
+            Our events provide an opportunity for members to volunteer, manage
+            and organise break out sessions.
+          </p>
+        </div>
       </div>
     </div>
   );
