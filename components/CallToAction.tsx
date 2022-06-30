548ec97f27c1a9b1f45d9dfc16ae91a598646c88
machariavck <victorkuria101@gmail.com>
2022-06-30 14:56:11 +0300

    minor improvements on #23

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -3,6 +3,7 @@ import { AiOutlineRise } from "react-icons/ai";
 import { ImEye } from "react-icons/im";
 import { GiPerson } from "react-icons/gi";
 
+
 const CallToAction = () => {
   return (
     <div className="my-[100px] bg-primary bg-opacity-10 h-auto rounded-3xl md:h-[446px] px-20 py-10">
@@ -12,46 +13,45 @@ const CallToAction = () => {
           Be a member
         </button>
       </div>
-      <div className="flex py-10 mx-auto flex-col md:flex-row items-center md:space-x-6 lg:space-x-10 h-auto">
-        {/* Value 1 */}
-        <div className="space-y-4 flex flex-col items-center md:items-start text-darkAccent w-auto">
-          <FaUsers className="h-16 text-primary w-16" />
-          <h1 className="font-semibold">Team work</h1>
-          <p className="text-center md:text-start">
-            We are bringing together women in our community to work together and
-            empower each other.
-          </p>
-        </div>
-        {/* Value 2 */}
-        <div className="space-y-4 flex flex-col items-center md:items-start text-darkAccent w-auto">
-          <AiOutlineRise className="h-16 text-primary w-16" />
-          <h1 className="font-semibold">Technical Growth</h1>
-          <p className="text-center md:text-start">
-            We provide technical and non-technical sessions to help our members
-            grow their skills.
-          </p>
-        </div>
-        {/* Value 3 */}
-        <div className="space-y-4 flex flex-col items-center md:items-start text-darkAccent w-auto">
-          <ImEye className="h-16 text-primary w-16" />
-          <h1 className="font-semibold">Visibility</h1>
-          <p className="text-center md:text-start">
-            We create visibility for our members through volunteer opportunities
-            and speaking sessions.
-          </p>
-        </div>
-        {/* Value 4 */}
-        <div className="space-y-4 flex flex-col items-center md:items-start text-darkAccent w-auto">
-          <GiPerson className="h-16 text-primary w-16" />
-          <h1 className="font-semibold">Leadership</h1>
-          <p className="text-center md:text-start">
-            Our events provide an opportunity for members to volunteer, manage
-            and organise break out sessions.
-          </p>
-        </div>
+      <div className="flex w-auto py-10 mx-auto flex-col md:flex-row items-start md:space-x-6 lg:space-x-10 h-auto">
+		{
+			item_contents.map(x => <Item Icon={x[0]} h3text={x[1]} ptext={x[2]} key={x[2]} />)
+		}
       </div>
     </div>
-  );
+  )
 };
+
+
+const item_contents = [
+	// icon, heading, paragraph
+	[
+		FaUsers,
+		"Team Work",
+		"We are bringing together women in our community to work together and empower each other."
+	], [
+		AiOutlineRise,
+		"Technical Growth",
+		"We provide technical and non-technical sessions to help our members grow their skills."
+	], [
+		ImEye,
+		"Visibility",
+		"We create visibility for our members through volunteer opportunities and speaking sessions."
+	], [
+		GiPerson,
+		"Leadership",
+		"Our events provide an opportunity for members to volunteer, manage and organise break out sessions."
+	]
+]
+
+const Item = ({h3text, ptext, Icon}: any) => {
+	return (
+		<div className="space-y-4 flex flex-col items-center md:items-start text-darkAccent">
+			<Icon className="h-16 text-primary w-16" />
+			<h3 className="font-semibold">{h3text}</h3>
+			<p className="text-center md:text-start">{ptext}</p>
+		</div>
+	)
+}
 
 export default CallToAction;
