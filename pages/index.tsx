59939452b8c1db5d7a6236b8f2ebccd2e71c2472
katungi <dankatdennis@gmail.com>
2022-07-11 18:36:35 +0300

    - added eventbrite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { Banner, Blog, CallToAction, EventsPane, Layout } from "../components";
 import posts from '../data/articles';
+import eventbrite from "eventbrite";
 
 export default function Home() {
+  useEffect(() => {
+    async function getEvents() {
+      let endpoint = 'https://www.eventbriteapi.com/v3/users/me/events'
+      let config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.EVENTBRITE_TOKEN}`
+        }
+      }
+      const events = await (await axios.get(endpoint, config)).data
+      console.log(events)
+    }
+    getEvents();
+  }, [])
+
   return (
     <div>
       <main className="w-full px-4 md:px-8 lg:px-20 text-darkAccent">
@@ -15,4 +32,8 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServersideProps() {
+
 }
